36fe301d3f2bf6a821d50982fd57b8817aa9ccaf
Philip Älgebrink <76400643+philipalgebrink@users.noreply.github.com>
2025-02-10 11:21:42 +0100

    Added spacer component
    * Might be used for aesthetics

---
+++ components/spacer.vue
@@ -0,0 +1,28 @@
+<template>  
+    <div class="spacer"></div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+.spacer {
+height: 10px;
+width: 100%;
+background: linear-gradient(to right, rgba(221, 101, 32, 0.5), transparent);
+margin: 10px 0;
+animation: fillSpacer 1.5s ease-out forwards;
+}
+
+@keyframes fillSpacer {
+  from {
+    width: 0;
+  }
+  to {
+    width: 100%;
+  }
+}
+
+</style>
